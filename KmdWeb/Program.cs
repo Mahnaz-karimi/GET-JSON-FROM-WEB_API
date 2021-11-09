e17e9204a5a9d4e9b035c118f78ed1ecee42b503
mz2020 <mahnaaz85@gmail.com>
2021-11-09 21:44:41 +0100

    modified timer time

--- KmdWeb/Program.cs
+++ KmdWeb/Program.cs
@@ -9,49 +9,40 @@ namespace KmdWeb
 
     {
         public static Timer newTimer = new Timer();
-        public static int interval_event_min = 3; // the time for timer event that check json-data and update dateabase
+        public static double interval_event = 180000.0; // the time of milliseconds for timer events, time for updating the database
         public static int reload_time = 2000; // time for reload data from sql database 2000 milisecend = 2 sec
 
-        public static int get_Minut_Difference_Json_from_sql(DateTime updatedAt) // diffrence between json and sql-database
+        public static void getNewTimer(Double jsont_difference_from_now) //  get new timer time for events.
         {
-            return Convert.ToInt32((updatedAt - DataHandling.getLastDateTimeFromDB(ConfigurationManager.AppSettings["connectionString"])).TotalMinutes); 
-        }
-
-        public static int get_Minut_Difference_Json_From_Now(DateTime updatedAt) // diffrence between json and datetime now
-        {         
-            return Convert.ToInt32((DateTime.Now - updatedAt).TotalMinutes);
-        }
-
-        public static void getNewTimer(int jsont_difference) //  get new timer time for events.
-        {
-            if (jsont_difference <= interval_event_min) // if the time difference between json and now is less or equal to the time that is estimated for updating the website
+            if (jsont_difference_from_now <= interval_event) // if the time difference between json and now is less or equal to the time that is estimated for updating the website
             {
-                int newIntervalInt = ((interval_event_min - jsont_difference) * 60 * 1000) + reload_time; 
+                Double newIntervalInt = interval_event - jsont_difference_from_now + reload_time; 
                 newTimer.Interval = newIntervalInt;
-                Console.WriteLine("\n New timer time : " + newIntervalInt / 60 / 1000 + "  Date time now: " + DateTime.Now);
+                Console.WriteLine("\n New timer time : " + newIntervalInt / 60 / 1000 + "  Date time now: " + DateTime.Now + "\n");
             }
-            else  // if the time difference between json and datetime-now is bigge then the time is estimated, timer will closes! 
+            else if (jsont_difference_from_now > interval_event && jsont_difference_from_now < 86400000)// if the time difference between json and datetime-now is bigger then the time is estimated, timer will sat 2 secend
             {
-                Console.WriteLine("\n Website currency exchange rate not updated, is  : " + jsont_difference + " minutter, Date time now: " + DateTime.Now);
-                newTimer.Close();
-            }
+                Console.WriteLine("\n Website currency exchange rate not updated, is: " + jsont_difference_from_now / 60 / 1000 + " minutter, Date time now: " + DateTime.Now);
+                newTimer.Interval = reload_time;
+            }            
 
         }
 
         public static void update_sql_TimerEvent(object source, ElapsedEventArgs e)
         {
             dynamic json = DataHandling.fetchData(); // fetch json from the website
+            DateTime update_At = json.updatedAt.Value;
             DataHandling.print_Json_From_URL(json); // print json file 
-            int difference_Json_SQl = get_Minut_Difference_Json_from_sql(json.updatedAt.Value); // difference between json and sql-database
-            Console.WriteLine("\n Difference between valutakurser website and our sql databases is: " + difference_Json_SQl + "\n");            
+            double difference_Json_SQl = Convert.ToDouble((update_At - DataHandling.getLastDateTimeFromDB(ConfigurationManager.AppSettings["connectionString"])).TotalMilliseconds); // difference between json and sql-database
+            Console.WriteLine("\n Difference between valutakurser website and our sql databases is: " + difference_Json_SQl + " ms = "+ difference_Json_SQl/60/1000+ " minetes \n");            
 
-            if (difference_Json_SQl > 0) // if there is time difference between json and sql, therefore data will saved.
+            if (difference_Json_SQl > 0) // if there is time difference between json and sql, data will be saved.
             {
                 DataHandling.insertJsonDataInSQl(json, ConfigurationManager.AppSettings["connectionString"]);
                 Console.WriteLine("\n SQL-database is updated. Date time now: " + DateTime.Now);
             }
-            // when program is here that means difference_Json_SQl is ziro, and we get new timer time
-            getNewTimer(get_Minut_Difference_Json_From_Now(json.updatedAt.Value)); 
+            // when the program is here, it means difference Json SQL will be zero and then we get new timer time          
+            getNewTimer(Convert.ToDouble((DateTime.Now - update_At).TotalMilliseconds)); // into getNewTimer method, throws time difference between json and now
         }
 
         public static void Main(string[] args)
