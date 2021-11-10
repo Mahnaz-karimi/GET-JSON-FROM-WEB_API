fb7d011cee05aab628999a2e325e0212119af1aa
mz2020 <mahnaaz85@gmail.com>
2021-11-11 00:00:01 +0100

    modify methods

--- KmdWeb/Program.cs
+++ KmdWeb/Program.cs
@@ -13,12 +13,10 @@ namespace KmdWeb
         public static int day_ms = 86400000; // milisecends in day
         public static int hour_ms = 3600000; //milisecendes in hour
 
-
-        public static Double get_new_IntervalInt_is_On_time(Double jsont_difference_from_now)
+        public static Double get_new_IntervalInt_when_json_is_On_time(Double jsont_difference_from_now)
         {
-            Double newIntervalInt = interval_event - jsont_difference_from_now + reload_time;
-            Console.WriteLine("\n Website currency exchange rate not updated, is: " + jsont_difference_from_now / 60 / 1000 + " minutter, Date time now: " + DateTime.Now);
-            Console.WriteLine("\n New timer time : " + newIntervalInt / 60 / 1000 + "  Date time now: " + DateTime.Now + "\n");
+            Double newIntervalInt = interval_event - jsont_difference_from_now + reload_time;            
+            Console.WriteLine("\n New timer time: " + newIntervalInt / 60 / 1000 + "\n");
             return newIntervalInt;
         }
 
@@ -30,16 +28,17 @@ namespace KmdWeb
         }
 
         public static Double getDifference_Json_Now(DateTime update_At)
-        {           
-            return Convert.ToDouble((DateTime.Now - update_At).TotalMilliseconds);
+        {    
+            Double dif = Convert.ToDouble((DateTime.Now - update_At).TotalMilliseconds);
+            Console.WriteLine("\n Valutakurser website is updated " + dif + " ms = " + dif / 60 / 1000 + " minetes ago \n");
+            return dif;
         }
 
         public static void getNewTimer(Double jsont_difference_from_now) //  get new timer time for events.
         {
-            if (jsont_difference_from_now <= interval_event) // if the time difference between json and now is less or equal to the time that is estimated, we get timer under 3 min
+            if (jsont_difference_from_now <= interval_event) // if the time difference between json and now is less or equal to the time that is estimated, we get timer on 3 min or less
             {
-                newTimer.Interval = get_new_IntervalInt_is_On_time(jsont_difference_from_now);           
-               
+                newTimer.Interval = get_new_IntervalInt_when_json_is_On_time(jsont_difference_from_now);               
             }
             else if (jsont_difference_from_now > interval_event && jsont_difference_from_now <= day_ms) // if the time difference between json and datetime-now is bigger than the time is estimated and less than one day, time will set 2 second
             {
@@ -47,10 +46,11 @@ namespace KmdWeb
                 newTimer.Interval = reload_time;
             }
             else if (day_ms < jsont_difference_from_now && jsont_difference_from_now < (day_ms * 7)) // if the time difference between json and datetime-now is bigger than one day and less to days, timer will sat one hour
-            {                
+            {
+                Console.WriteLine("\n "+ jsont_difference_from_now / day_ms + " day has passed and the website has not been updated: " + jsont_difference_from_now / 60 / 1000 + " minutter, Date time now: " + DateTime.Now);
                 newTimer.Interval = hour_ms; // interval sets to one hour
             }
-           
+ 
         }
 
         public static void save_In_Sql_If_There_Is_Difference(Double difference_Json_SQl, dynamic json)
@@ -65,16 +65,14 @@ namespace KmdWeb
 
         public static void update_sql_TimerEvent(object source, ElapsedEventArgs e)
         {
-
             dynamic json = DataHandling.fetchData(); // fetch json from the website
             DateTime update_At = json.updatedAt.Value;
             DataHandling.print_Json_From_URL(json); // print json file
-            Double difference_Json_SQl = getDifference_Json_SQl(update_At);
-            save_In_Sql_If_There_Is_Difference(difference_Json_SQl, json);
-            
+            Double difference_Json_SQl = getDifference_Json_SQl(update_At); // difference betwwen website and our datebase
+            save_In_Sql_If_There_Is_Difference(difference_Json_SQl, json); // if there is time difference between json and sql, json-data will be saved in sql.          
             // when the program is here, it means difference Json SQL is zero and then we get new timer time for update         
-            getNewTimer(getDifference_Json_Now(update_At)); // get difference from json and now and calculate interval
-                                                             
+            getNewTimer(getDifference_Json_Now(update_At)); // get difference from json and now and calculate interval for new timer events
+
         }
 
         public static void Main(string[] args)
