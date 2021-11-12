f74d86ab71f7160259e8db27cad1ed48728e5142
mz2020 <mahnaaz85@gmail.com>
2021-11-12 05:02:06 +0100

    added test get new timer

--- TestTimer/UnitTest1.cs
+++ TestTimer/UnitTest1.cs
@@ -11,10 +11,10 @@ namespace TestTimer
     [TestClass]
     public class UnitTest1
     {
-        double interval_event_test = 180000.0; // milliseconds for timer-events, database update time
-        int reload_time_test = 3000; // time for reload data from sql database 2000 milisecend = 2 sec
-        int day_ms_test = 86400000; // milisecends in day
-        int hour_ms_test = 3600000; //milisecendes in hour
+        double interval_event_test = 180.0; // milliseconds for timer-events, database update time
+        int reload_time_test = 3; // time for reload data from sql database 2000 milisecend = 2 sec
+        int day_ms_test = 86400; // milisecends in day
+        int hour_ms_test = 3600; //milisecendes in hour
 
         [TestMethod]
         public void TestGetDifference()
@@ -29,7 +29,8 @@ namespace TestTimer
             Assert.AreEqual(Convert.ToInt64(TimerCalculate.getDifference_Json_Now(updateTimeTest)), Convert.ToInt64(d));
             
         }
-        public void IntervalInt_when_json_is_On_time()
+        [TestMethod]
+        public void TestIntervalInt_when_json_is_On_time()
         {
             dynamic jsonTest;
             using (WebClient wctest = new WebClient())
@@ -40,8 +41,19 @@ namespace TestTimer
             Double d = Convert.ToDouble((DateTime.Now - updateTimeTest).TotalSeconds);
             Assert.AreEqual(TimerCalculate.get_new_IntervalInt_when_json_is_On_time(d), interval_event_test-d + reload_time_test);
         }
-
-
+        [TestMethod]
+        public void Test_Get_New_Timer_Time()
+        {
+            dynamic jsonTest;
+            using (WebClient wctest = new WebClient())
+            {
+                jsonTest = JsonConvert.DeserializeObject(wctest.DownloadString("C:/Users/Mahnaz/Desktop/GET_JSON_FROM_WEB_API/TestTimer/currencies.json"));
+            }
+            DateTime updateTimeTest = jsonTest.updatedAt.Value;
+            Double d = Convert.ToDouble((DateTime.Now - updateTimeTest).TotalSeconds);
+            Assert.AreEqual(TimerCalculate.get_New_Timer_Time(System.Math.Abs(d)), reload_time_test);
+        }
+        
     }
     
 }
